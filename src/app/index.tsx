37e92bafb3668531d11f5dcdcb67c0c104cff454
akira2709 <145766714+akira2709@users.noreply.github.com>
2024-11-08 18:23:02 +0300

    Add files via upload

---
+++ src/app/index.tsx
@@ -0,0 +1,7 @@
+
+
+export const App = () => {
+	return (
+		<div>1</div>
+	)
+}
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+  	"./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+		screens: {
+			'sm': '0px',
+			'md': '670px',
+			'big': '850px',
+			'lg': '1200px'
+		},
+		colors: {
+			"asphalt": {
+				DEFAULT: "#1c1e25",
+				"light": "#323542"
+			},
+		}
+  },
+  plugins: [],
+}
